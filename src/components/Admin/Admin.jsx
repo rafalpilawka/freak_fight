59ce64aa4751cd7e374d88558fad7bd2434fa71f
RAF <raf@MacBook-Pro-RAF.local>
2019-10-28 07:46:05 +0100

    Problem with context and adding fighters solved

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,167 +1,25 @@
 import React, { useContext, useState, useEffect, createContext } from 'react';
-import { Form, FormGroup, Label } from 'reactstrap';
-import styled from 'styled-components';
-import FirebaseContext from 'Firebase/FreakFight/context'
 import FighterContextProvider from 'Components/Admin/Context/FighterContext'
 import {FighterContext} from 'Components/Admin/Context/FighterContext'
 import FighterForm from 'Components/Admin/FighterForm';
-import {Route} from 'react-router-dom'
-import { statement } from '@babel/template';
-import { Store } from 'tough-cookie';
+import FightForm from 'Components/Admin/FightForm'
+import FightContextProvider from 'Components/Admin/Context/FightContext';
 
-const FormContainer = styled.div`
-	 {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
-		heigth: 500px;
-		width: 100%;
-	}
-`;
 
-const FighterContainer = styled.div`
-	 {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		background-color: black;
-		color: white;
-		margin: 25px;
-		padding: 30px;
-		border-radius: 5px;
-		width: 100%;
-	}
-`;
 
-const SubmitContainer = styled.button`
-	 {
-		padding: 8px;
-		font-size: 1.5rem;
-		text-transform: uppercase;
-		font-family: 'Teko', sans-serif;
-		color: white;
-		background-color: black;
-		border-radius: 5px;
-		position: relative;
-
-		&:hover {
-			background-color: darkred;
-			color: #fdec6e;
-			box-shadow: 0px 0px 5px #fdec6e;
-			-moz-transition: all 0.2s ease-in;
-			-o-transition: all 0.2s ease-in;
-			-webkit-transition: all 0.2s ease-in;
-			transition: all 0.1s ease-in;
-		}
-	}
-`;
-
-const Input = styled.input`
-	font-size: 16px;
-	border: solid 1px #dbdbdb;
-	border-radius: 3px;
-	color: #262626;
-	padding: 7px 33px;
-	border-radius: 3px;
-	color: #999;
-	cursor: text;
-	font-size: 1.5rem;
-	font-weight: 300;
-	text-align: center;
-	background: #fafafa;
-	&:active,
-	&:focus {
-		text-align: left;
-		outline: none !important;
-		border: 1px solid red;
-		box-shadow: 0 0 10px red;
-	}
-`;
-
-const FormGroupContainer = styled.div`
-	 {
-		display: flex;
-		width: 100%;
-		flex-direction: column;
-		align-items: center;
-		padding: 10px;
-		flex-grow: 1;
-		flex-basis: 1;
-		text-transform: uppercase;
-		font-family: 'Teko', sans-serif;
-		font-size: 1.5rem;
-	}
-`;
 
 const Admin = () => {
 
-	// const {fighter1, fighter2} = useContext(FighterContext)
-
-	const [fight, setFight] = useState({
-		favFighter: [],
-		favFighter: [],
-		fighter1: {},
-		fighter2: {},
-		votesForFav: [],
-		votesForWin: [],
-		winFighter1: [],
-		winFighter2: []
-  });
-
-	const updateFightHandler = e => {
-    e.preventDefault();
-    console.log(fight[`${e.target.id}`])
-    const edit = { ...fight };
-		edit[e.target.id] = e.target.value;
-    setFight({ ...edit });
-	};
-
-	const addFightSubmit = e => {
-
-    e.preventDefault();
-		console.log(fight)
-		
-		// addFighterHandler(fight);
-		// setFight(
-		// 	{
-		// 		favFighter: [],
-		// 		favFighter: [],
-		// 		fighter1: {},
-		// 		fighter1Photo: '',
-		// 		fighter2: {},
-		// 		fighter2Photo: '',
-		// 		votesForFav: [],
-		// 		votesForWin: [],
-		// 		winFighter1: [],
-		// 		winFighter2: []
-		// 	},
-		// );
-  };
-  
-  const formHelper = (fighter, data) =>{
-    console.log('zz99', fighter, data)
-    setFight({
-      ...fight,
-      [`${fighter}`]: data
-    })
-  }
 
 	return (
-		<Form style={{ width: '50%' }}>
-			<FormContainer className="FormContainer">
-				<SubmitContainer onClick={addFightSubmit.bind(this)}>
-					Add fight
-				</SubmitContainer>
-				<FighterContextProvider >	
-				 	<FighterForm id='fighter1'/>
-				</FighterContextProvider>
-				<FighterContextProvider >	
-				 	<FighterForm id='fighter2'/>
-				</FighterContextProvider>
-			</FormContainer>
-		</Form>
-
+		<FightContextProvider>
+		<FighterContextProvider>
+			<FightForm>
+				<FighterForm id='fighter1' />
+				<FighterForm id='fighter2' />
+			</FightForm>
+		</FighterContextProvider>
+		</FightContextProvider>
 	);
 };
 
--- src/components/Admin/FightForm.jsx
+++ src/components/Admin/FightForm.jsx
@@ -1,15 +1,121 @@
-import React, {useContext} from 'react';
+import React, { useContext, useEffect } from 'react';
 import { FightContext } from 'Components/Admin/Context/FightContext';
-import { FighterContext } from 'Components/Admin/Context/FighterContext'
+import { FighterContext } from 'Components/Admin/Context/FighterContext';
+import { Form } from 'reactstrap';
+import styled from 'styled-components';
+import FirebaseContext from 'Firebase/FreakFight/context';
 
-const FightForm = () => {
-  const {fight}= useContext(FightContext)
-  const {fighter1, fighter2, setfighter1, setfighter2} = useContext(FighterContext)
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const FormContainer = styled.div`
+	 {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		heigth: 500px;
+		width: 100%;
+	}
+`;
 
-export default FightForm
+const SubmitContainer = styled.button`
+	 {
+		padding: 8px;
+		font-size: 1.5rem;
+		text-transform: uppercase;
+		font-family: 'Teko', sans-serif;
+		color: white;
+		background-color: black;
+		border-radius: 5px;
+		position: relative;
+
+		&:hover {
+			background-color: darkred;
+			color: #fdec6e;
+			box-shadow: 0px 0px 5px #fdec6e;
+			-moz-transition: all 0.2s ease-in;
+			-o-transition: all 0.2s ease-in;
+			-webkit-transition: all 0.2s ease-in;
+			transition: all 0.1s ease-in;
+		}
+	}
+`;
+
+const Input = styled.input`
+	font-size: 16px;
+	border: solid 1px #dbdbdb;
+	border-radius: 3px;
+	color: #262626;
+	padding: 7px 33px;
+	border-radius: 3px;
+	color: #999;
+	cursor: text;
+	font-size: 1.5rem;
+	font-weight: 300;
+	text-align: center;
+	background: #fafafa;
+	&:active,
+	&:focus {
+		text-align: left;
+		outline: none !important;
+		border: 1px solid red;
+		box-shadow: 0 0 10px red;
+	}
+`;
+
+const FightForm = props => {
+	const { addFighterHandler } = useContext(FirebaseContext);
+	const { fight, setFight } = useContext(FightContext);
+	const { fighter1, fighter2, setfighter1, setfighter2 } = useContext(
+		FighterContext
+	);
+
+	useEffect(
+		() => {
+			addFighterHandler(fight);
+		},
+		[fight]
+	);
+
+	const updateFightHandler = e => {
+		e.preventDefault();
+		const edit = { ...fight };
+		edit[e.target.id] = e.target.value;
+		setFight({ ...edit });
+	};
+
+	const addFightSubmit = async e => {
+		e.preventDefault();
+		const edit = { ...fight };
+		edit.fighter1 = fighter1;
+		edit.fighter2 = fighter2;
+		setfighter1({
+			name: '',
+			nick: '',
+			weight: '',
+			height: '',
+			last_fight: '',
+			fighterImg: ''
+		});
+		setfighter2({
+			name: '',
+			nick: '',
+			weight: '',
+			height: '',
+			last_fight: '',
+			fighterImg: ''
+		});
+		setFight({ ...edit });
+	};
+
+	return (
+		<Form style={{ width: '50%' }}>
+			<FormContainer className="FormContainer">
+				<SubmitContainer onClick={addFightSubmit.bind(this)}>
+					Add fight
+				</SubmitContainer>
+				{props.children}
+			</FormContainer>
+		</Form>
+	);
+};
+
+export default FightForm;
