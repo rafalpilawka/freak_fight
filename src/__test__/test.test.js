1f65e165fa59e1430f13417afea3a6cb474d2c68
Piotr Ludwig <piotr.a.ludwig@gmail.com>
2019-11-07 09:05:28 +0100

    test added

---
+++ src/__test__/test.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+
+
